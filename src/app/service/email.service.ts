2fdd757459caa31cd9e361467a547c57b2a4770c
Parth Patil <parth253pp@outlook.com>
2025-02-11 08:06:58 +0530

    Nn

--- src/app/service/email.service.ts
+++ src/app/service/email.service.ts
@@ -1,3 +1,19 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class EmailService {
+//   private baseUrl:String="http://localhost:8282"
+//   constructor(private http:HttpClient) { }
+//   sendEmail(data:any){
+//     // return this.http.post(`${this.baseUrl}/sendemail`,data)
+//     console.log(data);
+//     return this.http.post(`${this.baseUrl}/sendemail`,data)
+//   }
+// }
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -5,11 +21,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EmailService {
-  private baseUrl:String="https://emailapi-seven.vercel.app"
-  constructor(private http:HttpClient) { }
-  sendEmail(data:any){
-    // return this.http.post(`${this.baseUrl}/sendemail`,data)
+  private baseUrl: string = "https://emailapi-seven.vercel.app"; // Use your Vercel backend URL
+
+  constructor(private http: HttpClient) { }
+
+  sendEmail(data: any) {
     console.log(data);
-    return this.http.post(`${this.baseUrl}/sendemail`,data)
+    return this.http.post(`${this.baseUrl}/sendemail`, data, {
+      headers: { 'Content-Type': 'application/json' }, // Ensure correct headers
+      withCredentials: false  // Disable credentials if not required
+    });
   }
 }
+
+//iwtu nncl jatz dmph
